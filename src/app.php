bb4dd21f496765089cf6d93269c89a0785f0f655
Aleksei Ivanov <contact@aleksei.dev>
2024-10-10 22:22:58 +0300

    Work on docker-compose.yml

--- src/app.php
+++ src/app.php
@@ -1,5 +1,27 @@
 <?php
 
+// ini_set('display_errors', 'off');
+ini_set('error_log', '/proc/self/fd/2');
+
 require __DIR__ . '/../vendor/autoload.php';
 
-echo rand(0, 100);
+use \bru\api\Client;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$bru_account = $_ENV['BRU_ACCOUNT'];
+$bru_app_id = $_ENV['BRU_APP_ID'];
+$bru_app_secret = $_ENV['BRU_APP_SECRET'];
+
+$api = new Client(
+    $bru_account,
+    $bru_app_id,
+    $bru_app_secret,
+);
+
+$stores = $api->request('get', 'stores');
+
+var_dump(
+    $api->request('get', 'stores')
+);
